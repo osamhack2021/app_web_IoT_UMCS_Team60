834c4147a5f808e8a89714678cf702887fac1d3f
Kyungmin Choi <76053639+2ood@users.noreply.github.com>
2021-09-13 14:20:50 +0000

    #5 added login page -only a dummy file

---
+++ APP/ucms/lib/login/login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Text("This is a login page."),
+      ),
+    );
+  }
+}
