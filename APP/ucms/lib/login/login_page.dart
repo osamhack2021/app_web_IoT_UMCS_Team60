42fdb5c246b4892e21f9f0c2226f8f74551ae0cd
Kyungmin Choi <76053639+2ood@users.noreply.github.com>
2021-09-14 14:35:54 +0000

    feat(app):#5 drew login_page structure

--- APP/ucms/lib/login/login_page.dart
+++ APP/ucms/lib/login/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -6,10 +7,46 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Text("This is a login page."),
-      ),
+        body: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Align(
+              child: ListView (
+                // ignore: prefer_const_literals_to_create_immutables
+                children : [
+                  const SizedBox(height:100),
+                   const Text("Login", style: TextStyle(fontWeight: FontWeight.bold), textAlign: TextAlign.center),
+                   const TextField(
+                     decoration: InputDecoration(
+                       hintText: "id",
+                     ),
+                   ),
+                   const TextField(
+                     obscureText:true,
+                     decoration: InputDecoration(
+                       hintText: "password",
+                     ),
+                   ),
+                  Row (
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children : [
+                    TextButton(onPressed: () {},
+                      child: const Text("용사 로그인", textAlign: TextAlign.center),
+                    ),
+                    TextButton(onPressed: () {},
+                      child: const Text("간부 로그인", textAlign: TextAlign.center),
+                   ),
+                    ],
+                  ),
+                  TextButton(onPressed: () {},
+                      child: const Text("회원가입", textAlign: TextAlign.center),
+                    ),
+                ],
+              ),
+            ),
+          ),
+        ),
     );
   }
 }
