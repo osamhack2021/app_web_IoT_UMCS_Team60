faad9d0090a9d136368716ceda2f74b2a11d9802
Myungseung Kim <54897403+mskim9967@users.noreply.github.com>
2021-09-17 15:45:19 +0000

    feat(web) : socket.io 구축 socket.io로 web과 server 통신하는 예제입니다 views/LocationState.vue 파일에 예시로 작성해놓았습니다
    Feats #16

--- WEB/src/views/LocationState.vue
+++ WEB/src/views/LocationState.vue
@@ -1,14 +1,42 @@
 <template>
   <div>
-    Person Location State Page
+    <h1>채팅방</h1>
+    <ul>
+      <li v-for="(msg, index) in messages" :key="index">{{msg}}</li>
+    </ul>
+    <div>
+      <input type="text" @keyup.enter="sendMessage()" v-model="message">
+      <button @click="sendMessage()">send</button>
+    </div>
+
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
-</script>
+import io from 'socket.io-client';
 
-<style>
-</style>
+export default {
+  name: 'room',
+  data() {
+    return {
+      messages: [],
+      // transports : ['websocket'] 옵션으로 cors 해결
+      socket : io('https://osamhack2021-app-web-iot-umcs-team60-pvpvjw57c9pxq-3002.githubpreview.dev', { transports : ['websocket'] })
+    }
+  },
+  methods: {
+    sendMessage() {
+      console.log("send to server: " + this.message);
+      this.socket.emit("chat message", {
+        message : this.message
+      });
+    }
+  },
+  mounted() {
+    this.socket.on('chat message', (data) => {
+      console.log("receive from server: " + this.message);
+      this.messages = [...this.messages, data.message];
+    });
+  }
+}
+</script>
