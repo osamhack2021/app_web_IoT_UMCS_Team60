3db0d60074fa368782e01570d5bb9990628c545c
Kyungmin Choi <76053639+2ood@users.noreply.github.com>
2021-09-21 13:39:54 +0000

    feat(app) : #27 done scaffolding user_assemble
    closed #27

--- APP/ucms/lib/theme/text_theme.dart
+++ APP/ucms/lib/theme/text_theme.dart
@@ -5,4 +5,6 @@ TextStyle h1() => const TextStyle (fontSize : 30, fontWeight: FontWeight.bold);
 TextStyle h2() => const TextStyle (fontSize : 25, fontWeight: FontWeight.bold);
 TextStyle h3() => const TextStyle (fontSize : 20, fontWeight: FontWeight.bold);
 
+TextStyle warning() =>const TextStyle (fontSize : 15, fontWeight: FontWeight.bold, color: Colors.red);
+
 TextStyle bold() => const TextStyle (fontWeight: FontWeight.bold);
