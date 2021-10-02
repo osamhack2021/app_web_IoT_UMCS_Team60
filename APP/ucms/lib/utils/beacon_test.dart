00528351b07795a2d74bd4d9050de0f8438d3a33
Myungseung Kim <54897403+mskim9967@users.noreply.github.com>
2021-10-02 13:17:30 +0900

    fix(app) ibeacon 테스트 코드 수정

--- APP/ucms/lib/utils/beacon_test.dart
+++ APP/ucms/lib/utils/beacon_test.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
-import 'package:beacons_plugin/beacons_plugin.dart';
-import 'package:flutter/material.dart';
 import 'package:ucms/components/custom_screen.dart';
 import 'package:ucms/components/texts.dart';
+
+import 'package:beacons_plugin/beacons_plugin.dart';
+import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
+import 'package:flutter/services.dart';
 
 class BeaconTest extends StatefulWidget {
   const BeaconTest({Key? key}) : super(key: key);
@@ -13,24 +16,77 @@ class BeaconTest extends StatefulWidget {
 
 class _BeaconTestState extends State<BeaconTest> {
   String _beaconResult = "";
+  var isRunning = false;
+
+  final StreamController<String> beaconEventsController = StreamController<String>.broadcast();
+
+  Future<void> initPlatformState() async {
+    if (Platform.isAndroid) {
+      await BeaconsPlugin.setDisclosureDialogMessage(
+        title: "Need Location Permission",
+        message: "This app collects location data to work with beacons."
+      );
+    }
+
+    BeaconsPlugin.listenToBeacons(beaconEventsController);
+
+    // iBeacon uuid 등록
+    await BeaconsPlugin.addRegion(
+        "testBeacon", "74278BDA-B644-4520-8F0C-720EAF059935");
+    
+    BeaconsPlugin.setForegroundScanPeriodForAndroid(
+        foregroundScanPeriod: 1000, foregroundBetweenScanPeriod: 10);
+
+    BeaconsPlugin.setBackgroundScanPeriodForAndroid(
+        backgroundScanPeriod: 1000, backgroundBetweenScanPeriod: 10);
+
+    beaconEventsController.stream.listen((data) {
+      if (data.isNotEmpty && isRunning) {
+        setState(() {
+          _beaconResult = data;
+        });
+
+        print(data);
+        // data 구조
+        // {
+        //   "name": "testBeacon",
+        //   "uuid": "74278bda-b644-4520-8f0c-720eaf059935",
+        //   "macAddress": "34:14:B5:41:A2:7E", <- uuid가 아닌 mac주소로 비콘 구분
+        //   "major": "4660",
+        //   "minor": "64001",
+        //   "distance": "0.32",
+        //   "proximity": "Immediate",
+        //   "scanTime": "02 October 2021 01:10:10 PM",
+        //   "rssi": "-47",
+        //   "txPower": "-59"
+        // }
+      }
+    }, onDone: () {}, onError: (error) {print("Error: $error");});
+
+    await BeaconsPlugin.runInBackground(true);
+
+    if (Platform.isAndroid) {
+      BeaconsPlugin.channel.setMethodCallHandler((call) async {
+        if (call.method == 'scannerReady') {
+          await BeaconsPlugin.startMonitoring();
+          setState(() {
+            isRunning = true;
+          });
+        }
+      });
+    } else if (Platform.isIOS) {
+      await BeaconsPlugin.startMonitoring();
+      setState(() {
+        isRunning = true;
+      });
+    }
+
+    if (!mounted) return;
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    final StreamController<String> beaconEventsController =
-        StreamController<String>.broadcast();
-    BeaconsPlugin.listenToBeacons(beaconEventsController);
-
-    beaconEventsController.stream.listen((data) {
-      setState(() {
-        _beaconResult += "stream listen 시작.\n이 아래에 beacon data 들이 찍힙니다.";
-      });
-      if (data.isNotEmpty) {
-        setState(() {
-          _beaconResult += (data + "\n");
-        });
-      }
-    }, onDone: () {}, onError: (error) {});
-
     return MaterialApp(
       home: KScreen(
         child: ListView(
@@ -39,6 +95,22 @@ class _BeaconTestState extends State<BeaconTest> {
             title("비콘 테스트"),
             const SizedBox(height: 20),
             Text(_beaconResult),
+            ElevatedButton(
+              onPressed: () async {
+                if (isRunning) {
+                  await BeaconsPlugin.stopMonitoring();
+                } else {
+                  initPlatformState();
+                  await BeaconsPlugin.startMonitoring();
+                }
+                setState(() {
+                  isRunning = !isRunning;
+                });
+              },
+              child:
+                Text(isRunning ? 'Stop Scanning' : 'Start Scanning',
+                style: TextStyle(fontSize: 20)),
+            )
           ],
         ),
       ),
