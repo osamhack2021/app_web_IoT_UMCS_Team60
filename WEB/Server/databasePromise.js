3523e7944f946a4082e3c583beead5fb1d019853
Myungseung Kim <54897403+mskim9967@users.noreply.github.com>
2021-10-05 03:26:20 +0000

    fix(db) db 접속 포트 변경 #41

--- WEB/Server/databasePromise.js
+++ WEB/Server/databasePromise.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  port: '3307',
+  host: '10.0.1.174',
+  port: '3306',
   user: 'root',
   password: process.env.DB_PASSWORD,
   database: 'UMCS'
