6d526ef5cad840fa70f617c3f1d50cb7151ec4c5
Myungseung Kim <54897403+mskim9967@users.noreply.github.com>
2021-10-06 16:02:00 +0000

    feat(api): api, socket 통신 테스트 페이지 구현
    Feats #41

---
+++ WEB/Server/routes/devRouter.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+const dbPromiseConnection = require(`../databasePromise`);
+
+function nowDateTime() {
+    return new Date().toISOString().slice(0, 19).replace('T', ' ');
+}
+
+function gaussianRandom() {
+    var v1, v2, s;
+  
+    do {
+      v1 = 2 * Math.random() - 1;  
+      v2 = 2 * Math.random() - 1;   
+      s = v1 * v1 + v2 * v2;
+    } while (s >= 1 || s == 0);
+  
+    s = Math.sqrt( (-2 * Math.log(s)) / s );
+  
+    return v1 * s;
+  }
+
+function temperatureGen() {
+    return 36.5 + gaussianRandom();
+}
+
+function timeGen() {
+    return "21:"+(20+Math.floor(Math.random()*20))+":"+Math.floor(Math.random()*60);
+}
+
+router.get('/makeAnomaly/:date', async (req, res) => {
+    let sql = "DELETE FROM anomaly WHERE DATE(reported_time)=?"; 
+    await dbPromiseConnection.query(sql, [req.params.date]);
+
+    sql = "SELECT * FROM user";
+    let [users] = await dbPromiseConnection.query(sql);
+
+    for(let user of users) {
+        let temperature = temperatureGen().toFixed(2);
+        let details = null;
+        if(temperature >= 38) details="고열, 기침";
+        if(temperature < 34.2) details="식은땀";
+         
+        sql = "INSERT INTO anomaly value (NULL, ?, ?, ?, ?)";
+        var [results] = await dbPromiseConnection.query(sql, [user.tag, temperature, details, req.params.date+ " " + timeGen()]);        
+    }
+});
+
+module.exports = router;
