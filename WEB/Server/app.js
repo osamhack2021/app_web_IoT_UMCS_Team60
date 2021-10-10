4f4f38797b0eb47c8022d77ad84449d778ffe7bd
mskim9967 <mskim9967@gmail.com>
2021-10-10 23:33:29 +0900

    fix(server) build 파일 경로 수정

--- WEB/Server/app.js
+++ WEB/Server/app.js
@@ -80,7 +80,7 @@ app.get('/', (req, res) => {
 
 
 app.get('/admin_page', (req, res) => {
-    res.sendFile(path.resolve(__dirname+'/build/index.html'));
+    res.sendFile(path.resolve(__dirname+'/public/index.html'));
 })
 
 app.get('/socketUser', (req, res) => {
--- WEB/umcs-web/vue.config.js
+++ WEB/umcs-web/vue.config.js
@@ -8,6 +8,6 @@ module.exports = {
     }
   },
   publicPath: process.env.NODE_ENV === "production" ? "./" : "",
-  outputDir: '../Server/build',
+  outputDir: '../Server/public',
   transpileDependencies: ["vuetify"],
 };
