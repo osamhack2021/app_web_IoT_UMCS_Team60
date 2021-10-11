e2cb7fd9c09e60937458d59e474eea80306ae37f
mskim9967 <mskim9967@gmail.com>
2021-10-11 21:41:59 +0900

    feat(api) : 이동/사용 미결재목록 조회기능 추가 api문서참조요망

--- WEB/Server/routes/api/facilityRequestRouter.js
+++ WEB/Server/routes/api/facilityRequestRouter.js
@@ -1,10 +1,17 @@
 const router = require('express').Router();
 const dbPromiseConnection = require(`../../databasePromise`);
+const managerAuth = require(`../../controllers/managerAuth`);
+
+function nowDate() {
+    let krDate = new Date();
+    krDate.setHours(krDate.getHours()+9);
+    return krDate.toISOString().slice(0, 10).replace('T', ' ');
+}
 
 router.get('/', async (req, res) => {
     var msg = {2:'not_found', 4: 'db_error'};
     try {
-        var sql = "SELECT * FROM facility_request"
+        var sql = "SELECT f.*, u.doom_id FROM facility_request f, user u WHERE f.user_tag=u.tag";
         var [results] = await dbPromiseConnection.query(sql);
         
         if(!results.length)
@@ -34,13 +41,9 @@ router.get('/', async (req, res) => {
 router.get('/search', async (req, res) => {
     var msg = {2:'not_found', 4: 'db_error'};
     try {
-        var sql = "SELECT * FROM facility_request"
-        if(Object.keys(req.query).length) {
-            sql += " WHERE";
-            for(key in req.query)
-                sql += ` ${key} = ? AND`;
-            sql = sql.substr(0, sql.length - 3);
-        }
+        var sql = "SELECT f.*, u.doom_id FROM facility_request f, user u WHERE f.user_tag=u.tag ";
+        for(key in req.query)
+            sql += `AND ${key} = ? `;
         var [results] = await dbPromiseConnection.query(sql, Object.values(req.query));
         
         if(!results.length)
@@ -67,10 +70,54 @@ router.get('/search', async (req, res) => {
 });
 
 
+router.get('/waiting_permission', managerAuth.checkLogin, async (req, res) => {
+    var msg = {2:'not_found', 3:'not_found',4: 'db_error'};
+    try {
+        let sql = "SELECT * FROM watchman WHERE manager_tags=? AND responsible_date=? ";
+        let [chargeDooms] = await dbPromiseConnection.query(sql, [req.user.tag, nowDate()]);
+
+        if(!chargeDooms.length)
+            return res.status(200).json({
+                code: 3,
+                msg: msg[3],
+            });
+
+        sql = "SELECT f.*, u.doom_id FROM facility_request f, user u WHERE f.user_tag=u.tag AND f.permission IS NULL ";
+        for(key in req.query)
+            sql += `AND ${key} = ? `;
+        var [results] = await dbPromiseConnection.query(sql, Object.values(req.query));
+        
+        if(!results.length)
+            return res.status(200).json({
+                code: 2,
+                msg: msg[2]
+            });
+        
+        // 오늘 근무지인 곳에서 온 요청만 조회
+        results = results.filter((e) => chargeDooms.find(ee=>ee.charge_doom === e.doom_id));
+
+        return res.status(200).json({
+            code: 1,
+            msg: "success",
+            total: results.length,
+            data: results
+        });
+
+    } catch(err) {
+        console.log(err)
+        return res.status(400).json({
+            code: 4,
+            msg: msg[4],
+            err
+        });
+    }
+});
+
+
 router.get('/:id', async (req, res) => {
     var msg = {2:'not_found', 4: 'db_error'};
     try {
-        var sql = "SELECT * FROM facility_request WHERE id=?"
+        var sql = "SELECT f.*, u.doom_id FROM facility_request f, user u WHERE f.user_tag=u.tag AND id=?"
         var [results] = await dbPromiseConnection.query(sql, [req.params.id]);
         
         if(!results.length)
